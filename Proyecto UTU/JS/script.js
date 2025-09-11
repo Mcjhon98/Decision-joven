45b9942e7436a25f9d1bc531a0a92aa6aea96d65
Mcjhon98 <emmilianogorgo98@gmail.com>
2025-09-10 22:30:17 -0300

    Add files via upload

---
+++ Proyecto UTU/JS/script.js
@@ -0,0 +1,19 @@
+function suma (a,b)
+{
+    return a + b;
+}
+
+function resta (a,b)
+{
+    return a - b;
+}
+
+function multiplica (a,b)
+{
+    return a * b;
+}
+
+function divide (a,b)
+{
+    return a / b;
+}   
